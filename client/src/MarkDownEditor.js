5e13d906f9a1bc33812744cf35cae201ae34411a
Thrgamer <stuartdann03@gmail.com>
2023-03-30 18:04:17 +0100

    More Editor Functionality
    Co-authored-by: Benjamin Kile <BenjaminKile@users.noreply.github.com>

--- client/src/MarkDownEditor.js
+++ client/src/MarkDownEditor.js
@@ -1,74 +1,116 @@
-const { marked } = require("marked")
+const { marked } = require("marked");
 
-let toggle = false
-let posCaretStart = 0
-let posCaretEnd = 0
+let toggle = false;
+let posCaretStart = 0;
+let posCaretEnd = 0;
+let result = document.getElementById("result");
+let textBox = document.getElementById("textBox");
+let editor = document.getElementById("editor");
+let myList = document.getElementById("myList");
 
 function parseMarked() {
-	toggle = !toggle
-	console.log(toggle)
+	toggle = !toggle;
+	console.log(toggle);
 	if (toggle) {
 		// Parse text into markdow
-		let textBox = document.getElementById("textBox")
-		let rawText = textBox.value
-		let markdown = marked.parse(rawText)
+		let rawText = textBox.value;
+		let markdown = marked.parse(rawText);
 
-		let result = document.getElementById("result")
-		result.innerHTML = markdown
-		textBox.style.display = "none"
+		result.innerHTML = markdown;
+		editor.style.display = "none";
 	} else {
-		textBox.style.display = "block"
-		result.innerHTML = null
+		editor.style.display = "block";
+		result.innerHTML = null;
 	}
 }
 
 function getCaret() {
-	posCaretStart = document.getElementById("textBox").selectionStart
-	posCaretEnd = document.getElementById("textBox").selectionEnd
-	console.log(posCaretStart, posCaretEnd)
-	return posCaretEnd, posCaretStart
+	posCaretStart = document.getElementById("textBox").selectionStart;
+	posCaretEnd = document.getElementById("textBox").selectionEnd;
+	console.log(posCaretStart, posCaretEnd);
+	return posCaretEnd, posCaretStart;
 }
 
 function bold() {
-	let textBox = document.getElementById("textBox")
-	let startString = textBox.value.substr(0, posCaretStart)
-	let endString = textBox.value.substring(posCaretEnd)
-	let substring = textBox.value.substring(posCaretStart, posCaretEnd)
-	textBox.value = startString + "**" + substring + "**" + endString
-	console.log(substring)
-	console.log(textBox.setSelectionRange(posCaretStart, posCaretEnd))
+	let textBox = document.getElementById("textBox");
+	let startString = textBox.value.substr(0, posCaretStart);
+	let endString = textBox.value.substring(posCaretEnd);
+	let substring = textBox.value.substring(posCaretStart, posCaretEnd);
+	textBox.value = `${startString} **${substring}** ${endString}`;
+	console.log(substring);
+	console.log(textBox.setSelectionRange(posCaretStart, posCaretEnd));
 }
 function italics() {
-	let textBox = document.getElementById("textBox")
-	let startString = textBox.value.substr(0, posCaretStart)
-	let endString = textBox.value.substring(posCaretEnd)
-	let substring = textBox.value.substring(posCaretStart, posCaretEnd)
-	textBox.value = startString + "*" + substring + "*" + endString
-	console.log(substring)
-	console.log(textBox.setSelectionRange(posCaretStart, posCaretEnd))
-}
-function link() {
-	let textBox = document.getElementById("textBox")
-	let startString = textBox.value.substr(0, posCaretStart)
-	let endString = textBox.value.substring(posCaretEnd)
-	let substring = textBox.value.substring(posCaretStart, posCaretEnd)
-	textBox.value = startString + "[" + substring + "](Insert-Link-Here)" + endString
-	console.log(substring)
-	console.log(textBox.setSelectionRange(posCaretStart, posCaretEnd))
-}
-function heading_1() {
-	let textBox = document.getElementById("textBox")
-	let startString = textBox.value.substr(0, posCaretStart)
-	let endString = textBox.value.substring(posCaretEnd)
-	let substring = textBox.value.substring(posCaretStart, posCaretEnd)
-	textBox.value = startString + "# " + substring + endString
-	console.log(substring)
-	console.log(textBox.setSelectionRange(posCaretStart, posCaretEnd))
+	let startString = textBox.value.substr(0, posCaretStart);
+	let endString = textBox.value.substring(posCaretEnd);
+	let substring = textBox.value.substring(posCaretStart, posCaretEnd);
+	textBox.value = `${startString} *${substring}* ${endString}`;
+	console.log(substring);
+	console.log(textBox.setSelectionRange(posCaretStart, posCaretEnd));
 }
 
-window.parseMarked = parseMarked
-window.getCaret = getCaret
-window.bold = bold
-window.italics = italics
-window.link = link
-window.heading_1 = heading_1
+// for links between notes
+function link() {
+	let startString = textBox.value.substr(0, posCaretStart);
+	let endString = textBox.value.substring(posCaretEnd);
+	let substring = textBox.value.substring(posCaretStart, posCaretEnd);
+	textBox.value = `${startString} [${substring}](Insert-Link-Here) ${endString}`;
+	console.log(substring);
+	console.log(textBox.setSelectionRange(posCaretStart, posCaretEnd));
+}
+function heading(event) {
+	let id = event[event.selectedIndex].id;
+	let startString = textBox.value.substr(0, posCaretStart);
+	let endString = textBox.value.substring(posCaretEnd);
+	let substring = textBox.value.substring(posCaretStart, posCaretEnd);
+	console.log(id);
+	textBox.value = `${startString} \n${"#".repeat(id)} ${substring} \n${endString}`;
+	console.log(substring);
+	console.log(textBox.setSelectionRange(posCaretStart, posCaretEnd));
+}
+
+function unList() {
+	let startString = textBox.value.substr(0, posCaretStart);
+	let endString = textBox.value.substring(posCaretEnd);
+	let substring = textBox.value.substring(posCaretStart, posCaretEnd);
+
+	const lines = substring.split("\n");
+	let newLines = [];
+	for (let i = 0; i < lines.length; i++) {
+		if (i >= posCaretStart && i <= posCaretEnd) {
+			newLines.push("- " + lines[i]);
+		} else {
+			newLines.push(lines[i]);
+		}
+	}
+	substring = newLines.join("\n");
+	textBox.value = `${startString}${substring}${endString}`;
+}
+
+function orList() {
+	let startString = textBox.value.substr(0, posCaretStart);
+	let endString = textBox.value.substring(posCaretEnd);
+	let substring = textBox.value.substring(posCaretStart, posCaretEnd);
+
+	const lines = substring.split("\n");
+	let newLines = [];
+	for (let i = 0; i < lines.length; i++) {
+		if (i >= posCaretStart && i <= posCaretEnd) {
+			newLines.push(`${i + 1}. ` + lines[i]);
+		} else {
+			newLines.push(lines[i]);
+		}
+	}
+	substring = newLines.join("\n");
+	textBox.value = `${startString}${substring}${endString}`;
+}
+//function toggle()
+
+window.parseMarked = parseMarked;
+window.getCaret = getCaret;
+window.bold = bold;
+window.italics = italics;
+window.link = link;
+window.heading = heading;
+window.unList = unList;
+window.orList = orList;
--- client/src/index.ts
+++ client/src/index.ts
@@ -3,8 +3,4 @@ import SimpleMDE from "simplemde/dist/simplemde.min"
 import "../node_modules/simplemde/dist/simplemde.min.css"
 import "./MarkDownEditor"
 
-setInterval(() => {
-	console.log("hello")
-}, 1000)
-
 // var simplemde = new SimpleMDE({ element: document.getElementById("test") });
